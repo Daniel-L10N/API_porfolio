986e95867037fbf43afd9fa2dcad5ac90023ab92
Daniel-L10N <alejandrolazolemus@gmail.com>
2024-02-14 20:30:07 -0500

    exelente

--- django_restframework/urls.py
+++ django_restframework/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 
 urlpatterns = [
+    path("", views.index, name="home"),
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
     path('docs/', include_docs_urls(title = "API Documentation"))
