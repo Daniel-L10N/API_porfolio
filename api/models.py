79b2ba9266ba39fbd16fbd42d66fb02c0d8916b8
Daniel-L10N <alejandrolazolemus@gmail.com>
2024-02-27 01:20:46 -0500

    creada function pree delete

--- api/models.py
+++ api/models.py
@@ -1,18 +1,40 @@
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 import os
 
-# Create your models here.
 class Programer(models.Model):
-    fullname = models.CharField(max_length = 100)
-    nickname = models.CharField(max_length = 50)
+    fullname = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=50)
     age = models.PositiveSmallIntegerField()
-    image = models.ImageField(upload_to='media/programers', null=True)
+    image = models.ImageField(upload_to='media/programers')
 
-    def delete(self, *args, **kwargs):
-        if os.path.isfile(self.image.path):
-            os.remove(self.image.path)
-        super(Programer, self).delete(*args, **kwargs)
+# Señal para manejar la eliminación de la imagen antes de que se elimine el objeto
+@receiver(pre_delete, sender=Programer)
+def delete_image(sender, instance, **kwargs):
+    # Elimina la imagen del almacenamiento si existe
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
 
 
-    def __str__(self):
-        return self.nickname
+# from django.db import models
+# from django.db.models.signals import pre_delete
+# import os
+
+# class Programer(models.Model):
+#     fullname = models.CharField(max_length=100)
+#     nickname = models.CharField(max_length=50)
+#     age = models.PositiveSmallIntegerField()
+#     image = models.ImageField(upload_to='media/programers')
+
+    
+
+#     def delete(self, *args, **kwargs):
+#         # Verificar si self.image existe para manejar el caso null=True
+#         if self.image and os.path.isfile(self.image.path):
+#             os.remove(self.image.path)
+#         super().delete(**kwargs)
+
+#     def __str__(self):
+#         return self.nickname
